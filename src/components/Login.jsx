e603629f2c44da46de91149a8b245c46d59a8b6a
sam <samueldazadaza@gmail.com>
2022-11-08 20:41:21 -0500

    se cambia estilo de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,28 +22,28 @@ function App() {
           <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
 
-              <h2 className="fw-bold mb-2 text-center">Sign in</h2>
+              <h2 className="fw-bold mb-2 text-center">Iniciar sesion en tienda Libros</h2>
               <p className="text-white-50 mb-3">Please enter your login and password!</p>
 
-              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg"/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Correo electronico' id='formControlLg' type='email' size="lg"/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Contraseña' id='formControlLg' type='password' size="lg"/>
 
-              <MDBCheckbox name='flexCheck' id='flexCheckDefault' className='mb-4' label='Remember password' />
+              <MDBCheckbox name='flexCheck' id='flexCheckDefault' className='mb-4' label='Recordar contraseña' />
 
               <MDBBtn size='lg'>
-                Login
+                Ingresar
               </MDBBtn>
 
               <hr className="my-4" />
 
               <MDBBtn className="mb-2 w-100" size="lg" style={{backgroundColor: '#dd4b39'}}>
                 <MDBIcon fab icon="google" className="mx-2"/>
-                Sign in with google
+                Ingresar con google
               </MDBBtn>
 
               <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
                 <MDBIcon fab icon="facebook-f" className="mx-2"/>
-                Sign in with facebook
+                Ingresar con facebook
               </MDBBtn>
 
             </MDBCardBody>
