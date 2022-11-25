541addadcb9daa06ead1cbbe7f4504b17ba186f9
sam <samueldazadaza@gmail.com>
2022-11-24 20:00:32 -0500

    probando db mongo en la nube

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   url: "mongodb://localhost:27017/grupo09"
+  //url : "mongodb+srv://grupo09:5RkNW3%.vYX%2gC@cluster0.p6frn6n.mongodb.net/libros"
+  //url : "mongodb+srv://grupo09:5RkNW3%.vYX%2gC@cluster0.p6frn6n.mongodb.net/test"
 };
